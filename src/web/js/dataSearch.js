71b5ab1b6634b4309e5b884af9ad8454f3e6450b
dbright <dbright@oneviewhealthcare.com>
2023-10-17 16:18:08 +0100

    handle empty dates

--- src/web/js/dataSearch.js
+++ src/web/js/dataSearch.js
@@ -13,14 +13,20 @@ const search = (items, names, periods, refDate) => {
     );
 }
 
-const getLatestTo = (item) => {
-    const supportedTo = new Date(item.supportedTo);
-    const supportedToExtended = new Date(item.supportedToExtended);
-    const useTo = new Date(item.useTo);
+const getLatestTo = (item) => {    
+    const supportedEndIsSet = item.supportedTo !== "";
+    const supportedTo = supportedEndIsSet ? new Date(item.supportedTo) : new Date(item.supportedFrom);
 
-    const dates = [supportedTo, supportedToExtended, useTo];
-    const sortedDates = dates.sort((a, b) => a - b);
-    return sortedDates[2];
+    const supportExtendedEndIsSet = item.supportedToExtended !== "";
+    const supportedToExtended = supportExtendedEndIsSet ? new Date(item.supportedToExtended) : supportedTo;
+    
+    const inUseEndIsSet = item.useTo !== "";
+    const useTo = inUseEndIsSet ? new Date(item.useTo) : supportedTo;
+
+    const dates = [supportedTo, supportedToExtended, useTo];    
+    const sortedDates = dates.sort((a, b) => b - a);
+    
+    return sortedDates[0];
 }
 
 export {
